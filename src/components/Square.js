79302dd292486811924621a226961ba96492e0f0
Carlie Duperval <carlierichell@gmail.com>
2022-05-29 15:42:22 -0400

    Added handleClick to Square file

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,12 +2,13 @@ import '../index.css';
 import { useState } from 'react';
 
 const Square = () => {
-
+// set useState as an empty string as his default value
     const [value, setVAlue] = useState("")
-
+// Set the value to X when you click
     const handelClick = () => {
         setVAlue ("X")
     }
+    // return the button 
     return <button className="square" onClick={ () => handelClick()}>{value}</button>;
   };
 
